8f2269ae6a4ac91552edb361a0b9198ce0d6557c
serchemach <serchemach@gmail.com>
2022-04-07 18:10:54 +0300

    Add react project

---
+++ task5/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { ClassExample } from './ClassExample/ClassExample';
+import { FunctionExample } from './FunctionExample/FunctionExample';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassExample />
+      <FunctionExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ task5/src/FunctionExample/FunctionExample.jsx
@@ -0,0 +1,8 @@
+import "./FunctionExample.css"
+
+export const FunctionExample = () =>{
+    return (<div>
+                <text>Super text</text>
+                <button>Mega button</button>
+            </div>)
+}
