20ee2211f843fdad98d7d8275274cf38f574de8e
serchemach <serchemach@gmail.com>
2022-05-03 20:32:26 +0300

    Add placeholder for empty AsteroidCardList

--- task9/src/AsteroidCardList/AsteroidCardList.jsx
+++ task9/src/AsteroidCardList/AsteroidCardList.jsx
@@ -6,17 +6,22 @@ import { AsteroidsContext } from "../App";
 export const AsteroidCardList = (props) => {
     const {list} = props;
     const {state, dispatch} = useContext(AsteroidsContext)
-
+    
+    
     let showList = list;
-
+    
     if(state.showMode === ASTEROID_SHOW_MODE_DANGEROUS){
         showList = showList.filter((item)=>item.grade==="опасен")
     }
-
+    
     console.log(useContext(AsteroidsContext))
-
-    return showList.map(
-        (item)=><AsteroidCard name={item.name} grade={item.grade} date={item.date}
-                distance={item.distance} size={item.size}/>)
-
+    
+    if(list.length === 0){
+        return <div>List is empty</div>
+    }
+    else{
+        return showList.map(
+            (item)=><AsteroidCard name={item.name} grade={item.grade} date={item.date}
+                    distance={item.distance} size={item.size}/>)
+    }
 }
