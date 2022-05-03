432b66269d78d645b5c5754848c9dece05a14a30
serchemach <serchemach@gmail.com>
2022-05-03 20:16:26 +0300

    Add global context and Destruction Page

---
+++ task9/src/DestructionPage/DestructionPage.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react"
+import { AsteroidsContext } from "../App"
+import { AsteroidCardList } from "../AsteroidCardList/AsteroidCardList"
+import { DestructionPageHeader } from "./DestructionPageHeader/DestructionPageHeader"
+import styles from "./DestructionPage.module.css"
+import { Navbar } from "../Navbar/Navbar"
+
+export const DestructionPage = () =>{
+    const {state, dispatch} = useContext(AsteroidsContext)
+    
+    return <div className={styles.destructionPage}>
+            <DestructionPageHeader />
+            <div className={styles.line} />
+            <Navbar showMode={state.showMode} distanceMode={state.distanceMode} dispatch={dispatch} />
+            <AsteroidCardList list={state.destructionList} />
+        </div>
+}
+
