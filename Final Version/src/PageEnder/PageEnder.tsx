84784dc84ba4a859cc9441433d8c8b3b27a2aa13
serchemach <49231956+serchemach@users.noreply.github.com>
2022-06-12 22:37:06 +0300

    Delete Final Version directory

--- Final Version/src/PageEnder/PageEnder.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./PageEnder.module.css";
-
-export const PageEnder = () => {
-    return (
-        <div className={styles.centerDiv}>
-            2021 © Все права и планета защищены
-        </div>
-    );
-};
